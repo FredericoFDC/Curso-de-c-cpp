5452db36a565d0d7b9bc80cbedfb89e883e60753
Frederico Ferrarezi <eng.producaoffdc@gmail.com>
2023-10-22 22:14:31 -0300

    Operacoes e Concatenar

---
+++ OperacoesMatematicas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    printf("Ola, seja bem vindo a nova aula \n");
+
+    //Definindo variaveis
+    int a = 6, b = 4;
+
+    printf("A Soma de %d e %d = %d \n", a, b, a+b);
+    printf("A Subtracao de %d e %d = %d \n", a, b, a-b);
+    printf("A Divisao de %d e %d = %d \n", a, b, a/b);
+    printf("A Multiplicacao de %d e %d = %d \n", a, b, a*b);
+    printf("O Resto da divisao de %d e %d = %d \n", a, b, a%b);
+    printf("O Valor absoluto de -3 = %d \n", abs(-3));
+
+    system("Pause");
+
+}
---
+++ imprimirlerconcatenar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    //def uma variavel
+    int a = 5, b;
+
+    printf("%d \n", a);
+
+    //Contatenando
+    printf("O valor da variavel a = %d \n", a);
+    // Mudando o valor de "a"
+    a = 20;
+    //Contatenando
+    printf("O valor da variavel a = %d \n", a);
+
+    scanf("%d", &b);
+
+    printf("O valor da variavel b = %d \n", b);
+
+    printf("uhum \n");
+
+    system("Pause");
+
+}
+
